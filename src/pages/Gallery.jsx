7c054fe2dc54dd36eb2c76b1910dc90f871c3a36
arjit <arjit6249@gmail.com>
2025-07-04 13:38:25 +0530

    fixed changes

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,11 +1,24 @@
 const Gallery = () => {
-    return (
-      <section id="gallery" className="p-8 bg-gray-50 grid sm:grid-cols-2 md:grid-cols-3 gap-4" data-aos="fade-up">
+  return (
+    <section
+      id="gallery"
+      className="p-8 bg-gray-50"
+      data-aos="fade-up"
+    >
+      <h2 className="text-3xl md:text-4xl font-bold text-yellow-600 mb-8 ">
+        Our Gallery
+      </h2>
+
+      <div className="max-w-7xl mx-auto grid sm:grid-cols-2 md:grid-cols-3 gap-4">
         {[...Array(6)].map((_, i) => (
-          <div key={i} className="h-48 bg-yellow-100 rounded shadow-md"></div>
+          <div
+            key={i}
+            className="h-48 bg-yellow-100 rounded shadow-md"
+          ></div>
         ))}
-      </section>
-    );
-  };
-  
-  export default Gallery;
+      </div>
+    </section>
+  );
+};
+
+export default Gallery;
